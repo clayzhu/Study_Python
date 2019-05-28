14e352c2c29a4d7011a6f626804999f5ad295595
zhuguang <zhuguang@corp.netease.com>
2019-05-29 00:41:53 +0800

    Create 6.2_迭代.py

---
+++ Demo/6. 高级特性/6.2_迭代.py
@@ -0,0 +1,15 @@
+def findMinAndMax(L):
+    if len(L) == 0:
+        return (None, None)
+
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
