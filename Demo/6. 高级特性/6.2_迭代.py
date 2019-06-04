ecc0a904476764fe6eb5a68e39351f429601eab0
zhuguang <zhuguang@corp.netease.com>
2019-06-04 17:39:39 +0800

    迭代求最小值、最大值实现。

--- Demo/6. 高级特性/6.2_迭代.py
+++ Demo/6. 高级特性/6.2_迭代.py
@@ -1,6 +1,23 @@
 def findMinAndMax(L):
     if len(L) == 0:
         return (None, None)
+    
+    min = 0
+    max = 0
+    for x in L:
+        if min == 0:
+            min = x
+        if max == 0:
+            max = x
+
+        if min > x:
+            min = x
+            continue
+        if max < x:
+            max = x
+            continue
+    
+    return (min, max)
 
 # 测试
 if findMinAndMax([]) != (None, None):
